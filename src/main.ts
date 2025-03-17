525e7f4ec3c0f233fab5f02526ea039c7a1caac5
PranaliShekokare-tech <hellbentstallion777@example.com>
2025-03-18 01:44:41 +0530

    added bootstrap logic and app module

---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() { 
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+  .setTitle('User & Document Management API')
+  .setDescription('APIs for User Management, Documents, and Ingestion')
+  .setVersion('1.0')
+  .addBearerAuth(
+    {
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'Authorization',
+      description: 'Enter JWT token',
+      in: 'header',
+    },
+    'access-token', 
+  )
+  .build();
+
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
